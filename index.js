761a6eb64c2ee4f00c9ab287419264f73a34db05
ahmadmujeeb33 <ahmadmujeeb333@gmail.com>
2021-06-21 20:10:50 -0400

    working on add employee

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const inquirer = require('inquire');
-const sql = require('mysql');
+const inquirer = require('inquirer');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Your password
+    password: '',
+    database: 'seed_DB',
+  });
+  
 
 
 
 const start = () =>{
 
     inquirer
-        prompt([
+        .prompt([
             
             {
                 type:'list',
@@ -19,35 +34,38 @@ const start = () =>{
 
         .then((response) =>{
 
-            switch(response){
-                case 'View all Employees':
-                    // something
-
-                case 'View all departments':
-                    // something 
-
-                case 'View all roles':
-                    // soemthign
-
-                case 'add departments':
-                    // soemthuih[onfd]
-                case 'add roles':
-                    //soemthign
-
-                case 'add employees':
-                    // soemthing
-                    addEmployee()
-
-                case 'Update Employee Roles':
-                    // something 
+            if(response.options === 'add employees'){
+                addEmployee()
             }
+                // case 'View all Employees':
+                //     // something
+
+                // case 'View all departments':
+                //     // something 
+
+                // case 'View all roles':
+                //     // soemthign
+
+                // case 'add departments':
+                //     // soemthuih[onfd]
+                // case 'add roles':
+                //     //soemthign
+
+                // case 'add employees':
+                //     // soemthing
+                //     console.log('in this');
+                   
+
+                // case 'Update Employee Roles':
+                //     // something 
+            
 
         })
 }
 
 function addEmployee(){
     inquirer
-        prompt([
+        .prompt([
             {
                 type:'input',
                 message: 'Please enter first name',
@@ -58,16 +76,9 @@ function addEmployee(){
                 type:'input',
                 message: 'Please enter last name',
                 name: "lname"
-            },
+            }
 
-            {
-                type:'list',
-                message: 'What is the employees role',
-                name: 'role',
-                choices: ['Manager','Software Engineer','Salesperson','Accountant','Lawyer']
-            },
-
-
+    
 
             // {
             //     type:'list',
@@ -77,9 +88,66 @@ function addEmployee(){
 
         ])
 
-        .next((response)=>{
-            if(response.role != 'Manager'){
+        .then((response)=>{
 
-            }
+            let roleChosen = getRoles();
+
+            const query = connection.query(
+                'INSERT INTO employee SET ?',
+                {
+                    first_name: response.fname,
+                    last_name: response.lname,
+                    
+
+                }
+            )
+            connection.end()
+        
+            
         })
 }
+
+function getRoles(){
+    connection.query('SELECT * FROM role ',(err,results) =>{
+        inquirer
+            .prompt([
+
+            {
+                type:'list',
+                message: 'What is the employees role',
+                name: 'role',
+                choices() {
+                    const choiceArray = [];
+                    results.forEach(({ title }) => {
+                      choiceArray.push(title);
+                    });
+                    console.log(choiceArray);
+                    return choiceArray;
+        
+                }
+        
+            },
+
+        ])
+
+        .then((response) =>{
+            return response.role;
+        })
+       
+    })
+}
+
+// function getManager(){
+//     con.query("SELECT * FROM customers", function (err, result, fields) {
+//         if (err) throw err;
+//         console.log(result);
+//       });
+// }
+
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}\n`);
+    start();
+  });
+  
